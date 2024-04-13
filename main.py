a15ef1856548800bccf35d45f06bb56e7001a5dd
Serhii Honchar <serhii.honchar.java@gmail.com>
2024-04-13 23:53:07 +0200

    feat: detect when the turtle collides with a car *squish*

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ screen.tracer(0)
 
 player = Player()
 car_manager = CarManager()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(player.go_up, "Up")
@@ -22,5 +23,12 @@ while game_is_on:
 
     car_manager.create_car()
     car_manager.move_cars()
+
+    # Detect collision with car
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
 
 screen.exitonclick()
--- scoreboard.py
+++ scoreboard.py
@@ -1,6 +1,16 @@
-FONT = ("Courier", 24, "normal")
+from turtle import Turtle
+FONT = ("Courier", 24, "bold")
+ALIGNMENT = "center"
 
 
 # Level: 2
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.hideturtle()
+        self.penup()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(f"GAME OVER", align=ALIGNMENT, font=FONT)
